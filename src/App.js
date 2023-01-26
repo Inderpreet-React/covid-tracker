49c458e12478bd36ff8d1a177ee4b183b3d3f326
Inderpreet <inderpreet.react.project@gmail.com>
2023-01-26 17:05:34 +0530

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { FormControl, MenuItem, Select } from "@mui/material";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+	const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
+
+	useEffect(() => {
+		const getCountriesData = async () => {
+			await fetch("https://disease.sh/v3/covid-19/countries")
+				.then((response) => response.json())
+				.then((data) => {
+					const countries = data.map((country) => ({
+						name: country.country,
+						value: country.countryInfo.iso2,
+					}));
+					setCountries(countries);
+				});
+		};
+		getCountriesData();
+	}, []);
 	return (
 		<div className="app">
-			<h1>Covid 19 Tracker</h1>
+			<div className="app__header">
+				<h1>Covid 19 Tracker</h1>
+				<FormControl className="app__dropdown">
+					<Select variant="outlined" value="abc">
+						{countries.map((country) => (
+							<MenuItem value={country.value}>{country.name}</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+			</div>
 		</div>
 	);
 }
